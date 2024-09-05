5c60f310e0f15d91e2c3898b87702484b1790bfa
[Anhelina Romanchuk] <[angelia.romanchuk@keyin.com]>
2024-09-05 11:35:36 -0230

    Lesson 1/ Welcom and culculate programs

---
+++ Java/Work/Lesson1/Addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner ; 
+public class Addition
+{ 
+	public static void main (String[] args)
+	{ 
+		Scanner input = new Scanner (System.in); 
+		int num1, num2, num3, culc; 
+		System.out.println ("Enter first number:"); 
+		num1 = input.nextInt(); 
+		System.out.println ("Enter second number:");
+		num2 = input.nextInt(); 
+		System.out.println ("Enter third number:"); 
+		num3 = input.nextInt(); 
+		culc = num1 * num2 + num3; 
+		System.out.printf ("sum = %d\n", culc); 
+	} 
+}	
+
